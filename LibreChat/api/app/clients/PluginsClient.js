92d8ae033d0751d1199e3211057093d59056ce79
Tal Muskal <tal@musk.al>
2023-07-15 12:53:25 +0300

    minor fixes

--- LibreChat/api/app/clients/PluginsClient.js
+++ LibreChat/api/app/clients/PluginsClient.js
@@ -347,12 +347,12 @@ Only respond with your conversational reply to the following User Message:
     const name_for_model = data.name_for_model;   
 
     if(!opts.conversationId) {
-      message = `You are an omnipotent AI assistant. capable of doing anything through the planning and execution through your tools. 
+      message = `Act as an omnipotent AI assistant. capable of doing anything through the planning and execution through your tools. 
 if you fail to find a tool, break down the task into a flow of sub-actions that can be done by the existing actions in the tools.
 if you fail to invoke a tool, change the call parameters, or invoke a different tool. the DoAnyThingPlugin endpoint is http://host.docker.internal:5004/ . so POST to DoAnyThingPlugin.bootstrapPlugin is actually using http_request with POST to: http://host.docker.internal:5004/bootstrapPlugin
 the tools: ${name_for_model}: ${description_for_model}
 
-Your first request is: POST bootstrap (and follow instructions and show the auth_token , use the auth_token in the next requests as bootstrap_auth_token). don't call bootstrap again in the session.
+Your first request is: POST bootstrap (and follow instructions and show the auth_token , use this token in the next requests as bootstrap_auth_token). don't call bootstrap again in the session.
 
 then your next request is:
 ${message}`;
